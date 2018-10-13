ef2c7d439cf3e007f95e8e666178f723918c4515
Yair <yairs_lu@lagash.com>
2018-10-13 00:07:23 -0300

    remove canvas test square

--- cartuchos/src/app/formation-detail/formation-detail.component.ts
+++ cartuchos/src/app/formation-detail/formation-detail.component.ts
@@ -8,32 +8,32 @@ import { Formation } from '../formation';
 })
 export class FormationDetailComponent implements OnInit, AfterViewInit {
   @Input() formation: Formation;
-  @ViewChild('myCanvas') myCanvas: ElementRef;
+  // @ViewChild('myCanvas') myCanvas: ElementRef;
   @ViewChild('myCanvas2') myCanvas2: ElementRef;
-  private context: CanvasRenderingContext2D;
+  // private context: CanvasRenderingContext2D;
   private context2: CanvasRenderingContext2D;
 
   ngOnInit() {
   }
 
   ngAfterViewInit(): void {
-    this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
+    // this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
 
-    this.context.beginPath();
-    this.context.moveTo(0, 0);
-    this.context.lineTo(1000, 500);
-    this.context.moveTo(1000, 0);
-    this.context.lineTo(0, 500);
-    this.context.moveTo(575, 250);
-    this.context.arc(500, 250, 75, 0, Math.PI * 2);
-    this.context.moveTo(250, 10);
-    this.context.lineTo(280, 50);
-    this.context.lineTo(310, 10);
-    this.context.moveTo(280, 50);
-    this.context.lineTo(280, 100);
-    this.context.stroke();
-    this.context.font = '100px Arial';
-    this.context.strokeText('CANVAS', 300, 450);
+    // this.context.beginPath();
+    // this.context.moveTo(0, 0);
+    // this.context.lineTo(1000, 500);
+    // this.context.moveTo(1000, 0);
+    // this.context.lineTo(0, 500);
+    // this.context.moveTo(575, 250);
+    // this.context.arc(500, 250, 75, 0, Math.PI * 2);
+    // this.context.moveTo(250, 10);
+    // this.context.lineTo(280, 50);
+    // this.context.lineTo(310, 10);
+    // this.context.moveTo(280, 50);
+    // this.context.lineTo(280, 100);
+    // this.context.stroke();
+    // this.context.font = '100px Arial';
+    // this.context.strokeText('CANVAS', 300, 450);
 
     this.context2 = (<HTMLCanvasElement>this.myCanvas2.nativeElement).getContext('2d');
 
@@ -49,6 +49,7 @@ export class FormationDetailComponent implements OnInit, AfterViewInit {
     this.context2.strokeRect(0, 100, 200, 300);
     this.context2.strokeRect(800, 100, 200, 300);
     this.context2.strokeRect(0, 150, 100, 200);
+    this.context2.strokeRect(900, 150, 100, 200);
     this.context2.stroke();
   }
 
